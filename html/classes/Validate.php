12863077b3e4185de14c7bbebc880e247b136600
ZacharyHebert <alwayskeepcontents@yahoo.com>
2014-05-05 19:49:22 -0700

    Added license blurb to files

--- html/classes/Validate.php
+++ html/classes/Validate.php
@@ -1,3 +1,16 @@
+/*
+Copyright 2014 Zachary Hebert
+This file is part of Methodocracy.org.
+
+    Methodocracy.org is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+    Methodocracy.org is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with Methodocracy.org.  If not, see <http://www.gnu.org/licenses/>.
+	
+    Methodocracy TM is a trademark of Methodocracy.org (C)2014, and all rights to that TM are reserved. Any modified versions are required to be marked as changed, so that their problems will not be attributed erroneously to authors of previous versions. And the name Methodocracy TM should be clearly labeled as the source of your work as long as any part of this work remains intact in part or in whole.
+*/
+
 <?php
 class Validate {
 	private $_passed = false,
