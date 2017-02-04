16c33746fda641954ffdca92d09ba3db9c7ada39
ZacharyHebert <alwayskeepcontents@yahoo.com>
2017-02-04 08:52:52 -0800

    Finished updating license in inportant files

--- projectfiles/contact-form-handler.php
+++ projectfiles/contact-form-handler.php
@@ -1,5 +1,5 @@
 <!--
-	Copyright 2014-2015 Zachary Hebert, Patrick Gillespie
+	Copyright 2014 Zachary Hebert, Patrick Gillespie
 	This file is part of Methodocracy.org.
 
     Methodocracy.org is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
@@ -8,7 +8,7 @@
 
     You should have received a copy of the GNU General Public License along with Methodocracy.org.  If not, see <http://www.gnu.org/licenses/>.
 	
-    Methodocracy TM is a trademark of Methodocracy.org (C)2014-2015, and all rights to that TM are reserved. Any modified versions are required to be marked as changed, so that their problems will not be attributed erroneously to authors of previous versions. And the name Methodocracy TM should be clearly labeled as the source of your work as long as any part of this work remains intact in part or in whole.
+    Methodocracy TM and methodocracy.org TM are trademarks of Methodocracy Foundation (C)2014, and all rights to that TM are reserved. Any modified versions are required to be marked as changed, so that their problems will not be attributed erroneously to authors of previous versions. And the name Methodocracy TM should be clearly labeled as the source of your work as long as any part of this work remains intact in part or in whole.
 -->
 <?php 
 $errors = '';
